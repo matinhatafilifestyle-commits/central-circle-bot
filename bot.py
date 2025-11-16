9ddd1ab58cf3140801b98f1187db0734a81745f3
matinhatafilifestyle-commits <matinhatafilifestyle@gmail.com>
2025-11-16 16:07:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-print("Bot is running successfully!")
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    print("Bot is running successfully!")
+    while True:
+        time.sleep(1)
